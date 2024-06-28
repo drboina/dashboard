af3dac8692f75a1223e33dba112a87bb66f74da5
dr. boina <152813972+drboina@users.noreply.github.com>
2024-06-27 23:33:40 -0700

    27 junio

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,36 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from colorspacious import cspace_converter # type: ignore
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
-import plotly.graph_objs as go
-theme_plotly = None 
+cmaps = {}
+
+gradient = np.linspace(0, 1, 256)
+gradient = np.vstack((gradient, gradient))
+
+def plot_color_gradients(category, cmap_list):
+    # Create figure and adjust figure height to number of colormaps
+    nrows = len(cmap_list)
+    figh = 0.35 + 0.15 + (nrows + (nrows - 1) * 0.1) * 0.22
+    fig, axs = plt.subplots(nrows=nrows + 1, figsize=(6.4, figh))
+    fig.subplots_adjust(top=1 - 0.35 / figh, bottom=0.15 / figh,
+                        left=0.2, right=0.99)
+    axs[0].set_title(f'{category} colormaps', fontsize=14)
+
+    for ax, name in zip(axs, cmap_list):
+        ax.imshow(gradient, aspect='auto', cmap=mpl.colormaps[name])
+        ax.text(-0.01, 0.5, name, va='center', ha='right', fontsize=10,
+                transform=ax.transAxes)
+
+    # Turn off *all* ticks & spines, not just the ones with colormaps.
+    for ax in axs:
+        ax.set_axis_off()
+
+    # Save colormap list for later.
+    cmaps[category] = cmap_list
 
 
 st.set_page_config(page_title="Reportes",page_icon="🌍",layout="wide")
@@ -56,11 +82,13 @@ if option == 'Licitaciones y Contratos':
     with side2:
         labels = df_proc.index
         sizes = df_proc.values
+        colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99']
         fig1, ax1 = plt.subplots()
-        ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-                shadow=False)
-        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        ax1.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%',
+                 shadow=True)
+        ax1.axis('equal')
         st.pyplot(fig1)
+        plt.show()
     
     st.markdown ('___________________________________________________')
     st.subheader("Proveedores de Contratos 2024")
