6f08b009b05a23e97da1641633ece848190a27be
boinas <leonardo.baez@sonora.gob.mx>
2024-06-28 23:01:12 -0700

    28 jun

--- dashboard.py
+++ dashboard.py
@@ -2,19 +2,11 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import numpy as np
-
-st.set_page_config(page_title="Reportes",page_icon="🌍",layout="wide")
+st.set_page_config(page_title="Reportes Ejecutivo", layout="wide")
 st.header("Sistema de Reportes del Ejecutivo")
 st.sidebar.image("logo.png")
 
-
-# load Style css
-with open('style.css')as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
-
-
-option = st.sidebar.selectbox("Opciones",('Licitaciones y Contratos','Presupuesto','Ordenes de pago','Minutario','Adquisiciones'))
+option = st.sidebar.selectbox("Opciones",('Licitaciones y Contratos','Presupuesto','Ordenes de pago','PAAAS'))
 
 st.header(option)
 
@@ -23,31 +15,28 @@ if option == 'Licitaciones y Contratos':
 
     st.subheader("Reporte de Contratos 2023")
     df=pd.read_excel("CONTRATOS_2023.xlsx", dtype={'PARTIDA' : str, 'CENTRO GESTOR' : str, 'MONTO TOTAL ADJUDICADO': float})  
- 
+
     no_contratos = len(pd.Series(df['CONTRATO']).value_counts())
     monto_total=float(pd.Series(df["MONTO TOTAL ADJUDICADO"].sum()))
     
     total1,total2 = st.columns(2,gap='small')
     
-    with total2:
-        st.info('Monto total adjudicado:',icon="💰")
-        st.metric(label="",value=f"{monto_total:,.0f}")
-
     with total1:
         st.info('No. Contratos')
         st.metric(label="",value=f"{no_contratos:,.0f}")
 
+    with total2:
+        st.info('Monto total adjudicado:')
+        st.metric(label="",value=f"${monto_total:,.0f} M.N.")
+
     st.markdown ('___________________________________________________')
     
-    df_proc=df.groupby(["TIPO CONTRATO"])["MONTO TOTAL ADJUDICADO"].sum().astype(int).sort_values(ascending=False)
-    df_proV=df.groupby(["PROVEEDOR"])["MONTO TOTAL ADJUDICADO"].sum().astype(int).sort_values(ascending=False)
-    df_partida=df.groupby(["PARTIDA"])["MONTO TOTAL ADJUDICADO"].sum().astype(int).sort_values(ascending=False)
-    df_ua=df.groupby(["UNIDAD"])["MONTO TOTAL ADJUDICADO"].sum().astype(int).sort_values(ascending=False)
-    
+    df_proc=df.groupby(["TIPO CONTRATO"])["MONTO TOTAL ADJUDICADO"].sum().astype(int).sort_values(ascending=False)    
+    st.subheader("Procedimientos de Contratación 2023")
     side1,side2 = st.columns(2,gap='small')
   
     with side1:
-        st.subheader("Procedimientos de Contratación 2023")
+        st.text('Tabla 1. Monto total por procedimiento')
         st.dataframe(df_proc)
 
     with side2:
@@ -55,26 +44,29 @@ if option == 'Licitaciones y Contratos':
         sizes = df_proc.values
         colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99']
         fig1, ax1 = plt.subplots()
-        ax1.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%',
-                 shadow=True)
+        ax1.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True)
         ax1.axis('equal')
         st.pyplot(fig1)
         plt.show()
+        st.text('Figura 1. Porcentaje por tipo de procedimiento')
     
     st.markdown ('___________________________________________________')
-    st.subheader("Proveedores de Contratos 2024")
+    df_proV=df.groupby(["PROVEEDOR"])["MONTO TOTAL ADJUDICADO"].sum().astype(int).sort_values(ascending=False)
+    st.subheader("Proveedores de Contratos 2023")
     st.dataframe(df_proV)
 
     st.markdown ('___________________________________________________')
-    st.subheader("Partidas del Gasto de Contratos 2024")
+    df_partida=df.groupby(["PARTIDA"])["MONTO TOTAL ADJUDICADO"].sum().astype(int).sort_values(ascending=False)
+    st.subheader("Gasto por Partida de Contratos 2023")
     st.dataframe(df_partida)
     
     st.markdown ('___________________________________________________')
-    st.subheader("Unidades Administrativas Contratos 2024")
+    df_ua=df.groupby(["UNIDAD"])["MONTO TOTAL ADJUDICADO"].sum().astype(int).sort_values(ascending=False)
+    st.subheader("Unidades Administrativas Contratos 2023")
     st.dataframe(df_ua)
     
 if option == 'Presupuesto':
-    st.subheader("Presupuesto de Egresos 2024")
+    st.subheader("Estado del Ejercicio del Ejecutivo")
     
     df=pd.read_excel("TECHO PPTAL 2024 EJECUTIVO.xlsx")
      
@@ -84,10 +76,7 @@ if option == 'Ordenes de pago':
     st.subheader("Status de Ordenes de pago")
 
 if option == 'Minutario':
-    st.subheader("Control de Oficios de la Coordinación Ejecutiva de Administración y Finanzas")
-
-if option == 'Adquisiciones':
-    st.subheader("Reporte de Adquisiciones 2024")
+    st.subheader("PAAAS")
 
 
 
